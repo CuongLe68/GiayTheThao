1b5c947977f2b0eaf155207fbb031b258a86d39e
CuongLe68 <cuongle.23012001@gmail.com>
2022-11-15 18:38:17 +0700

    upload lightbox

--- index.js
+++ index.js
@@ -28,3 +28,28 @@ document.querySelector('.btn-prev').onclick = () => {
         sileContainer.style.transform = `translateX(${IMG_WIDTH * index}rem)`
     }
 }
+
+//hover imgs
+var items = document.querySelectorAll('.container-row-item>.container-row-item-lightbox')
+var item = document.querySelector('.container-row-item-lightbox')
+
+for (let j = 0; j < items.length; j++) {
+    items[j].onmouseover = function(e) {
+        e.preventDefault();
+        
+        for (let z = 0; z < items.length; z++) {
+            items[z].classList.remove('active')            
+        }
+
+        this.classList.add('active')
+        
+        var imgs = document.querySelectorAll(".active>.container-row-item-colors>img");
+        for(let i = 0; i < imgs.length; i++) {
+            imgs[i].onmouseover = function(e) {
+                e.preventDefault();
+                var maining = document.querySelector('.container-row-item-lightbox.active>img');
+                maining.src = this.src;  
+            }    
+        }
+    }
+}
